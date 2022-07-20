c36420d02b9e6ad93a20f2d5ce7a6c964c7ea069
DEBBINDU BAIRAGI <69134738+debbairagi@users.noreply.github.com>
2022-07-20 08:55:35 +0600

    delete cpp text

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+
+// echo "# ProgrammingPractice" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M 'main'
+
+// git push -u origin 'main'
