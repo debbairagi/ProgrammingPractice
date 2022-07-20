692af96a952ad7daa47be906cce5b8da92c600c8
DEBBINDU BAIRAGI <69134738+debbairagi@users.noreply.github.com>
2022-07-20 10:10:57 +0600

    new install

---
+++ SEM-2.1/D-20.07.2022/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float a, b;
+    cin >> a >> b;
+    float avg = (a + b)/2;
+
+    cout << "MEDIA = " << avg << endl;
+    return 0;
+}
